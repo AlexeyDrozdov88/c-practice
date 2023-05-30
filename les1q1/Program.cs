815beb57246c9c2fe0594975d453896a8e299da9
Alexey Drozdov <lexxa.lol@gmail.com>
2023-05-30 19:25:38 +0300

    addles1q1

---
+++ les1q1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+//показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+Console.WriteLine("Введи трехзначное число");
+int number = int.Parse(Console.ReadLine());
+
+void FindSecondDigit(int num) //ищет вторую цифру
+{
+    if (number > 99 && number < 1000)
+    {
+        int SecondDigit = (number / 10) % 10;
+        Console.WriteLine("Вторая цифра - " + SecondDigit);
+    }
+    else
+    {
+        Console.WriteLine("Некорректный ввод числа");
+    }
+}
+
+
+FindSecondDigit(number);
